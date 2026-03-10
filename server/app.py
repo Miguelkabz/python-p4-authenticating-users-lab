5712fba6ca9c3ebc004d3ac726ece0b476d8ec6d
Miguelkabz <kabzmiguel@gmail.com>
2026-03-10 10:41:18 +0300

    done

--- server/app.py
+++ server/app.py
@@ -20,6 +20,13 @@ api = Api(app)
 
 class ClearSession(Resource):
 
+    def get(self):
+    
+        session['page_views'] = None
+        session['user_id'] = None
+
+        return {}, 204
+
     def delete(self):
     
         session['page_views'] = None
@@ -48,9 +55,34 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter(User.username == username).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {'error': 'Invalid username'}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = db.session.get(User, user_id)
+            return user.to_dict(), 200
+        return {}, 401
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
